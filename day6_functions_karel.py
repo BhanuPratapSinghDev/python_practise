4440b0b608541b9a63170dc790f7c6d17c0913c1
BhanuPratapSinghDev <bhanupratapofficial23@gmail.com>
2025-05-23 12:03:51 +0530

    Add files via upload

---
+++ day6_functions_karel.py
@@ -0,0 +1,14 @@
+# Practise on Reeborg's world ( similar to karel which has a robot in a grid based world)
+
+# Functions : just a wrapper name for a block of code helps to save time
+# Defining a function (parenthesis differentiate variable from function )
+# for indentation 4 spaces generally or a tab
+def my_function():
+    print("Hello")
+    print("Bye")
+my_function()       # calling a function
+
+# While loop (more dangerous than for loop)
+# 1 while something_is_true :
+# 2   do somthing repeatedly           1 -> 2-> 1-> 2-> 1 ........so on
+
