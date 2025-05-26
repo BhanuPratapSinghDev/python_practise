dbbe76697eec0ea609cb0611560e3f249fc66a9f
BhanuPratapSinghDev <bhanupratapofficial23@gmail.com>
2025-05-26 23:56:53 +0530

    Add files via upload

---
+++ day9_dictionary_nesting.py
@@ -0,0 +1,6 @@
+# Dictionary = { key : value} eg { "name" : "bhanu" , "age" : 19}
+programming_dictionary= {
+    "Bug":"defect in the program causing it to behave in an undesirable manner ", # to make it clear we write with 1 indent
+    "Functions":"Peice of code that you can easily call over and over again"
+    }
+print(programming_dictionary["Bug"])            # KeyError : " Bog"
